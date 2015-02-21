0a25688eb54e8043e721506ed50119d367ecc47f
Rahul Kumar Dubey <rkd7474@gmail.com>
2015-02-22 01:28:40 +0530

    direct commit

---
+++ src/main/java/com/learning/patterns/DirectCommitRepo.java
@@ -0,0 +1,12 @@
+package com.learning.patterns;
+
+/**
+ * Created by Rahul Dubey on 2/22/2015.
+ */
+public class DirectCommitRepo {
+    public static void main(String[] args) {
+        System.out.println("Directly Commiting to Hitawa Repo as I rkd7474 is the Collaborator.");
+        System.out.println("rkd7474 Can Directly Checkout without forking ");
+    }
+
+}
