8365b1598efa3f7490c32a4f51ddde9bc9308650
Rahul Kumar Dubey <rkd7474@gmail.com>
2015-02-21 23:52:37 +0530

    Rajat Added for Testing

--- src/main/java/com/learning/patterns/Test.java
+++ src/main/java/com/learning/patterns/Test.java
@@ -5,6 +5,6 @@ package com.learning.patterns;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world Rajat");
     }
 }
